46ed0c53fbd9cd88549bfb9a3b76d579b0a7cd2d
Luke Miller <lukealexmiller@gmail.com>
2017-11-29 00:02:06 +0100

    further small dir changes

--- create_mscoco_tfrecord.sh
+++ create_mscoco_tfrecord.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
+
 # Specify data root and Syno mount point
-DATA_ROOT='/data/mscoco'
-DATA_ZIP="${DATA_ROOT}/zips"
+DATA_ROOT='root/data/mscoco'
+SYNO_ZIP="${DATA_ROOT}/zips"
+
 # Specify MSCOCO dataset year
 YEAR='2014'
+
+# Get data from MSCOCO
+wget "http://images.cocodataset.org/zips/train${YEAR}.zip" -O "${SYNO_ZIP}"
+wget "http://images.cocodataset.org/zips/val${YEAR}.zip" -O "${SYNO_ZIP}"
+wget "http://images.cocodataset.org/zips/annotations_trainval${YEAR}.zip" -O "${SYNO_ZIP}"
 
 # Construct arguments to pass to python
 ARGS="--data_root=${DATA_ROOT} --output_root=${DATA_ROOT} --year=${YEAR} "
 ARGS+="--shuffle=True --num_val=8000 --train_shards=64 --val_shards=4 --test_shards=0 --num_threads=8"
 
 # Install MSCOCO API
-if [ ! -d "/data/scripts/cocoapi" ]; then 
+if [ ! -d "/root/data/scripts/cocoapi" ]; then 
   ./install_mscoco_api.sh ; 
 fi  && \
 
 # Unzip data located on Syno to local directories
-echo "$(unzip ${DATA_ZIP}/val${YEAR}.zip -d ${DATA_ROOT})" && \
-echo "$(unzip ${DATA_ZIP}/train${YEAR}.zip -d ${DATA_ROOT})" && \
-echo "$(unzip ${DATA_ZIP}/annotations_trainval${YEAR}.zip -d ${DATA_ROOT})" && \
+echo "$(unzip ${SYNO_ZIP}/val${YEAR}.zip -d ${DATA_ROOT})" && \
+echo "$(unzip ${SYNO_ZIP}/train${YEAR}.zip -d ${DATA_ROOT})" && \
+echo "$(unzip ${SYNO_ZIP}/annotations_trainval${YEAR}.zip -d ${DATA_ROOT})" && \
 
 # Create TF Records
 echo "$(python ./create_mscoco_tf_record.py ${ARGS})"
--- install_mscoco_api.sh
+++ install_mscoco_api.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-echo "Cloning cocoapi into /data/scripts" && \
-cd /data/scripts && \
+echo "Cloning cocoapi into /root/data/scripts" && \
+cd /root/data/scripts && \
 git clone https://github.com/cocodataset/cocoapi && \
 cd  cocoapi/PythonAPI/ && \
 pip install Cython && \
