36e6da9d815c4ef29b99168edb9996be8ad17128
Luke Miller <lukealexmiller@gmail.com>
2017-11-29 00:25:08 +0100

    edits

--- create_mscoco_tfrecord.sh
+++ create_mscoco_tfrecord.sh
@@ -9,11 +9,12 @@ YEAR='2014'
 
 # Get data from MSCOCO
 if [ ! -d "${SYNO_ZIP}" ]; then 
-  echo "$(mkdir -p ${SYNO_ZIP})" && \
-  echo "$(wget http://images.cocodataset.org/zips/train${YEAR}.zip -O ${SYNO_ZIP})" && \
-  echo "$(wget http://images.cocodataset.org/zips/val${YEAR}.zip -O ${SYNO_ZIP})" && \
-  echo "$(wget http://images.cocodataset.org/zips/annotations_trainval${YEAR}.zip -O ${SYNO_ZIP})" ;
+  echo "$(mkdir -p ${SYNO_ZIP})" ;
 fi  && \
+
+echo "$(wget http://images.cocodataset.org/zips/train${YEAR}.zip -O ${SYNO_ZIP})" && \
+echo "$(wget http://images.cocodataset.org/zips/val${YEAR}.zip -O ${SYNO_ZIP})" && \
+echo "$(wget http://images.cocodataset.org/zips/annotations_trainval${YEAR}.zip -O ${SYNO_ZIP})" && \
 
 # Construct arguments to pass to python
 ARGS="--data_root=${DATA_ROOT} --output_root=${DATA_ROOT} --year=${YEAR} "
